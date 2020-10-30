36e9d520a52c22d087ecd4b74e3b0a14dfce6a7d
Nguyen Duc Manh <manhnd.jin@gmail.com>
2020-10-30 15:09:01 +0700

    setup project structure

---
+++ src/helpers/events.ts
@@ -0,0 +1,5 @@
+export class Events {
+  constructor() {}
+
+  setupListener(): void {}
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import './polyfills';
 import { Utils } from './helpers/utils';
 
 export class MediumEditor {
+  editor: HTMLElement | null;
   utils = new Utils();
-  constructor(elemId: string, otps: any) {}
+
+  constructor(elemId: string, otps: any) {
+    this.editor = document.getElementById(elemId);
+
+    if (!this.editor) {
+      throw new Error(`Cannot find element with id ${elemId}`);
+    }
+
+    if (this.editor.tagName !== 'DIV') {
+      console.warn('We recommend editor container is a div');
+    }
+  }
 }
